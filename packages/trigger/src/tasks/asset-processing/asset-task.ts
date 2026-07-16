c698f90931eb0b2623f88a836897008a737c02e2
Leonardo Motta <mail@leomotta.me>
2026-07-16 11:23:03 -0300

    refactor(asset-processing): remove obsolete asset processing files to streamline codebase

--- packages/trigger/src/tasks/asset-processing/asset-task.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-export const assetTaskPayloadSchema = z.object({
-  assetId: z.string(),
-  organizationId: z.string(),
-})
-
-export type AssetTaskPayload = z.infer<typeof assetTaskPayloadSchema>
